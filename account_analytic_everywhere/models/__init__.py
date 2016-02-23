162ee0cdaec32d0e5c95693dc66fae48eb07f61f
Samuel <777_samuel@live.com.mx>
2016-02-23 12:49:02 -0600

    [ADD] Commit inicial

---
+++ account_analytic_everywhere/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import account_invoice
+from . import mrp_production
+from . import purchase_order
+from . import stock_move
+from . import stock_picking
