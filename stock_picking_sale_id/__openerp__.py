cd1216ba0f31b13456990da0a4dc63e61487d052
Deysy <damp_0508@hotmail.com>
2016-03-08 15:55:19 -0600

    [ADD]Add module stock_picking_sale_id

---
+++ stock_picking_sale_id/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking - Sale Order Reference",
+    "summary": "Add a field in the picking referencing to its sale_order",
+    "version": "9.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://odoo-community.org/",
+    "author": "<Deysy Mascorro, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base",
+        "stock",
+        "sale",
+    ],
+    "data": [
+        "views/stock_picking_view.xml",
+    ],
+    "demo": [
+
+    ],
+    "qweb": [
+
+    ]
+}
