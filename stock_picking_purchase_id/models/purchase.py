c1f9a508d92a4b7a36245898bbb87e5edc128b99
Deysy <damp_0508@hotmail.com>
2016-03-08 09:18:30 -0600

    [ADD]Add module stock_picking_purchase_id

---
+++ stock_picking_purchase_id/models/purchase.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _name = 'purchase.order'
+    _inherit = 'purchase.order'
+
+    @api.model
+    def _prepare_picking(self):
+        purchase_vals = super(PurchaseOrder, self)._prepare_picking()
+        purchase_vals['purchase_id'] = self.id
+
+        return purchase_vals
