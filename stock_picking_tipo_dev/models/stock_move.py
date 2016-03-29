fef9fde6444b4bf804ced0bf8322e9a56ba8a467
Samuel <777_samuel@live.com.mx>
2016-03-29 17:04:41 -0600

    [ADD] module stock_picking_tipo_dev

---
+++ stock_picking_tipo_dev/models/stock_move.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import _, fields, models
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    dev_tipo = fields.Selection(
+        [('normal', _(u'Normal')),
+         ('rebilling', _(u'Rebilling')),
+         ('cancellation', _(u'Cancellation'))],
+        default='normal',
+        string=_(u"Type"),
+        help=_("defines the type")
+    )
