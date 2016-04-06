47bf88a1db7a76476b67ad13f155cbf844b3c81a
Samuel <777_samuel@live.com.mx>
2016-04-06 09:49:25 -0500

    [FIX] Add field return_account_id and change of model

--- res_settings_freight/models/res_config.py
+++ res_settings_freight/models/res_config.py
@@ -7,7 +7,7 @@ from openerp import SUPERUSER_ID
 
 
 class SaleConfiguration(models.TransientModel):
-    _inherit = 'sale.config.settings'
+    _inherit = 'account.config.settings'
 
     freight_account_id = fields.Many2one(
         'account.account',
@@ -19,12 +19,17 @@ class SaleConfiguration(models.TransientModel):
         string=_(u'Default account installation'),
     )
 
+    return_account_id = fields.Many2one(
+        'account.account',
+        string=_(u'Default account sales return'),
+    )
+
     @api.v7
     def set_freight_account_id_defaults(self, cr, uid, ids, context=None):
         freight_account_id = self.browse(
             cr, uid, ids, context=context).freight_account_id
         res = self.pool.get('ir.values').set_default(
-            cr, SUPERUSER_ID, 'sale.config.settings',
+            cr, SUPERUSER_ID, 'account.config.settings',
             'freight_account_id', freight_account_id.id)
         return res
 
@@ -33,6 +38,15 @@ class SaleConfiguration(models.TransientModel):
         installation_account_id = self.browse(
             cr, uid, ids, context=context).installation_account_id
         res = self.pool.get('ir.values').set_default(
-            cr, SUPERUSER_ID, 'sale.config.settings',
+            cr, SUPERUSER_ID, 'account.config.settings',
             'installation_account_id', installation_account_id.id)
+        return res
+
+    @api.v7
+    def set_return_account_id_defaults(self, cr, uid, ids, context=None):
+        return_account_id = self.browse(
+            cr, uid, ids, context=context).return_account_id
+        res = self.pool.get('ir.values').set_default(
+            cr, SUPERUSER_ID, 'account.config.settings',
+            'return_account_id', return_account_id.id)
         return res
