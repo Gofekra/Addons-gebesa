8512a384abdd63ab27347dbbcdf73757f7df0f54
Samuel <777_samuel@live.com.mx>
2016-04-29 11:31:27 -0500

    [ADD] module stock_picking_account_location

---
+++ stock_picking_account_location/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock picking account location",
+    "summary": "Stock picking account location",
+    "version": "9.0.1.0.0",
+    "category": "Account",
+    "website": "https://odoo-community.org/",
+    "author": "<Samuel Barron>, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base", "stock_account", "stock_location_account_account",
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
