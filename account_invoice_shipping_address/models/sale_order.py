1ba3a0535e41e32530a2d6b5c1d91b1bf492e380
Deysy <damp_0508@hotmail.com>
2016-06-03 16:15:46 -0500

    [ADD] Add new module account_invoice_shipping_address

---
+++ account_invoice_shipping_address/models/sale_order.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def _prepare_invoice(self):
+        ## ---> Set BreakPoint
+        import pdb;
+        pdb.set_trace()
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals['partner_shipping_id'] = self.partner_shipping_id.id
+
+        return invoice_vals
