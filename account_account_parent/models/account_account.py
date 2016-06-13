311e0d50e52b75de2d8d102b6897380bf1e5394b
Cesar Barron <cesar.barron@gmail.com>
2016-06-13 13:39:09 -0500

    [FIX] Added new method to get the account level in the whole chart of accounts

--- account_account_parent/models/account_account.py
+++ account_account_parent/models/account_account.py
@@ -2,11 +2,25 @@
 # © <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import _, fields, models
+from openerp import _, api, fields, models
 
 
 class AccountAccount(models.Model):
     _inherit = 'account.account'
+    _name = 'account.account'
+
+    @api.multi
+    def _get_account_level(self, level):
+        """
+        get the account level in the whole account chart
+        """
+        self.ensure_one()
+        level += 1
+
+        if self.parent_id and self.parent_id.id:
+            return self.parent_id._get_account_level(level)
+        else:
+            return level
 
     parent_id = fields.Many2one(
         'account.account',
