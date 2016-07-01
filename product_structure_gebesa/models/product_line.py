b877366e80392c80031b560e8a1a5d386fb7b9df
Samuel <777_samuel@live.com.mx>
2016-07-01 17:51:27 -0500

    [ADD] module product_structure_gebesa

---
+++ product_structure_gebesa/models/product_line.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import _, fields, models
+
+
+class ProductLine(models.Model):
+    _name = 'product.line'
+    _description = 'product.line'
+    _order = "name asc"
+
+    code = fields.Char(
+        string=_('Code'),
+        size=5,
+        required=True,
+        help=_('Line code product'),
+    )
+
+    name = fields.Char(
+        string=_('Name'),
+        size=120,
+        required=True,
+        help=_('Line name product'),
+    )
+
+    product_family_id = fields.Many2one(
+        'product.family',
+        string=_('Product Family'),
+    )
+
+    active = fields.Boolean(
+        string=_('Active'),
+        default=True
+    )
