5afa1f03cfd6a5096a1cba03a0c44d575090fd46
Samuel <777_samuel@live.com.mx>
2016-09-20 12:20:40 -0500

    [FIX] add exception

--- account_payment_auth/models/account_invoice.py
+++ account_payment_auth/models/account_invoice.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import _, api, fields, models
+from openerp.exceptions import ValidationError
 
 
 class AccountInvoice(models.Model):
@@ -31,17 +32,26 @@ class AccountInvoice(models.Model):
     @api.multi
     def action_payment_auth_request(self):
         for invoice in self:
+            if not invoice.authorizes_id.id:
+                raise ValidationError(_("Error!\nField Authorized it's not \
+                                      valid"))
             invoice.aut_estatus_pago = 'proposed'
         return True
 
     @api.multi
     def action_payment_auth(self):
         for invoice in self:
+            if self.env.uid != invoice.authorizes_id.id:
+                raise ValidationError(_("Error!\nYou may not authorize this \
+                                      payment is not assigned to you"))
             invoice.aut_estatus_pago = 'authorized'
         return True
 
     @api.multi
     def action_refuse_payment(self):
         for invoice in self:
+            if self.env.uid != invoice.authorizes_id.id:
+                raise ValidationError(_("Error!\nYou can not refuse this \
+                                      payment is not assigned to you"))
             invoice.aut_estatus_pago = 'rejected'
         return True
