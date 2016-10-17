beb666d50faca74b883593ccb016d66153f484bd
Samuel <777_samuel@live.com.mx>
2016-10-17 13:58:57 -0500

    [FIX] add model account_voucher

---
+++ account_invoice_number_hist/models/account_voucher.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class AccountVoucher(models.Model):
+    _inherit = 'account.voucher'
+
+    number = fields.Char(
+        readonly=False,
+        copy=False
+    )
