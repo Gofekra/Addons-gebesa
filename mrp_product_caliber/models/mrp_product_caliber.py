3a216971ce12716f8323ec1b8c371377c0a74dcd
Deysy <damp_0508@hotmail.com>
2016-10-25 16:14:30 -0500

    [FIX] Add new field in module mrp_product_caliber

--- mrp_product_caliber/models/mrp_product_caliber.py
+++ mrp_product_caliber/models/mrp_product_caliber.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import _, models, fields
+from openerp.addons import decimal_precision as dp
 
 
 class MrpProductCaliber(models.Model):
@@ -17,6 +18,26 @@ class MrpProductCaliber(models.Model):
 
     name_caliber = fields.Char(
         string=_('Name Caliber'),
+    )
+
+    espesor_mm = fields.Float(
+        string=_('Density MM'),
+        digits_compute=dp.get_precision('Product Unit of Measure'),
+    )
+
+    espesor_pgs = fields.Float(
+        string=_('Density PGS'),
+        digits_compute=dp.get_precision('Product Unit of Measure'),
+    )
+
+    peso_kg = fields.Float(
+        string=_('Weight KG'),
+        digits_compute=dp.get_precision('Product Unit of Measure'),
+    )
+
+    peso_lb = fields.Float(
+        string=_('Weight LB'),
+        digits_compute=dp.get_precision('Product Unit of Measure'),
     )
 
     _sql_constraints = [
