eedfd381772f19ff3ac7246ec777deead36297db
Samuel <777_samuel@live.com.mx>
2016-10-25 12:53:49 -0500

    [FIX] datetime

--- multicurrency_mexico/models/res_currency_rate.py
+++ multicurrency_mexico/models/res_currency_rate.py
@@ -5,6 +5,45 @@
 from openerp import _, api, fields, models
 
 
+class ResCurrency(models.Model):
+    _inherit = 'res.currency'
+
+    def _get_current_rate(self, cr, uid, ids, name, arg, context=None):
+        if context is None:
+            context = {}
+        res = {}
+
+        date = context.get('date') or fields.Datetime.now()
+        company_id = context.get('company_id') or self.pool[
+            'res.users']._get_company(cr, uid, context=context)
+        for id in ids:
+            cr.execute("""SELECT rate FROM res_currency_rate
+                           WHERE currency_id = %s
+                             AND CAST(name AS DATE) <= %s
+                             AND (company_id is null
+                                 OR company_id = %s)
+                        ORDER BY company_id, name desc LIMIT 1""",
+                       (id, date, company_id))
+            if cr.rowcount:
+                res[id] = cr.fetchone()[0]
+            else:
+                res[id] = 1
+        return res
+
+    def _get_conversion_rate(self, cr, uid, from_currency,
+                             to_currency, context=None):
+        if context is None:
+            context = {}
+        ctx = context.copy()
+        ctx['date'] = ctx['date'] + ' 23:00:00'
+        ## ---> Set BreakPoint
+        import pdb;
+        pdb.set_trace()
+        from_currency = self.browse(cr, uid, from_currency.id, context=ctx)
+        to_currency = self.browse(cr, uid, to_currency.id, context=ctx)
+        return to_currency.rate / from_currency.rate
+
+
 class ResCurrencyRate(models.Model):
     _inherit = 'res.currency.rate'
 
