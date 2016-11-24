7f0726c2d187329a9e729fc9662aab2ef0af2e7a
Cesar Barron <cesar.barron@gmail.com>
2016-11-24 12:33:16 -0600

    [FIX] Dependency missing and pylint

--- integration_cost_gebesa/__openerp__.py
+++ integration_cost_gebesa/__openerp__.py
@@ -19,6 +19,7 @@
         "account",
         "account_invoice_stock_picking_id",
         "account_analytic_everywhere",
+        "stock_landed_cost"
     ],
     "data": [
         "security/security.xml",
