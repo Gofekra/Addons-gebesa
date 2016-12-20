cd5afe99c894f678bbb7300ac6ccb5b810477023
Samuel <777_samuel@live.com.mx>
2016-12-20 15:48:10 -0600

    [IMP] data for invoice

--- account_analytic_everywhere/models/sale_order.py
+++ account_analytic_everywhere/models/sale_order.py
@@ -2,18 +2,22 @@
 # © <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, models
+from openerp import api, fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     @api.multi
-    def _prepare_invoice(self):
-        invoice_vals = super(SaleOrder, self)._prepare_invoice()
-        invoice_vals['sale_id'] = self.id
+    def action_invoice_create(self, grouped=False, final=False):
 
-        return invoice_vals
+        res = super(SaleOrder, self).action_invoice_create(
+            grouped, final)
+        invoice = self.env['account.invoice'].browse(res)
+        for inv in invoice:
+            inv.account_analytic_id = inv.sale_id.project_id
+        return res
+
 
 class SaleAdvancePaymentInv(models.TransientModel):
     _inherit = "sale.advance.payment.inv"
@@ -23,5 +27,5 @@ class SaleAdvancePaymentInv(models.TransientModel):
         res = super(SaleAdvancePaymentInv, self)._create_invoice(
             order, so_line, amount)
         for inv in res:
-            inv.sale_id = order.id
+            inv.account_analytic_id = order.project_id
         return res
