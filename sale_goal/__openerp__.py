20ff6e49c3d0534af6b20eb00b40f3092432ec96
Samuel <777_samuel@live.com.mx>
2017-01-03 12:20:31 -0600

    [ADD] module sale_goal

---
+++ sale_goal/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sales goals",
+    "summary": "Sales goals.",
+    "version": "9.0.1.0.0",
+    "category": "Sales",
+    "website": "https://odoo-community.org/",
+    "author": "<Samuel Barron>, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base", "sale",
+        "sales_channel",
+    ],
+    "data": [
+        "views/sale_goal.xml",
+        "views/res_partner.xml",
+        "security/security.xml",
+        "security/ir.model.access.csv"
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
