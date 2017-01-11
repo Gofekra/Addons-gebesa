1207915f59def1ecd2841c793f7b7f2b7c451144
Samuel <777_samuel@live.com.mx>
2017-01-11 13:37:57 -0600

    [IMP] account move stock inventory

--- stock_location_account_account/models/stock_location.py
+++ stock_location_account_account/models/stock_location.py
@@ -2,7 +2,7 @@
 # © <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import _, fields, models
+from openerp import _, api, fields, models
 
 
 class StockLocation(models.Model):
@@ -12,3 +12,9 @@ class StockLocation(models.Model):
         'account.account',
         string=_(u'Inventory account'),
     )
+
+    @api.model
+    def _location_owner(self, location):
+        ''' Return the company owning the location if any '''
+        return location and (
+            location.usage != 'view') and location.company_id or False
