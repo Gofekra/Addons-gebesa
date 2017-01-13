9c9c3f62b1ca1e481f7f22abd3230c6db615f3d7
Samuel <777_samuel@live.com.mx>
2017-01-13 17:19:58 -0600

    [ADD] module stock_picking_batch

---
+++ stock_picking_batch/models/stock_picking.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def force_transfer(self, force_qty=True):
+        for pick in self:
+            for pack in pick.pack_operation_ids:
+                if not pack.qty_done > 0:
+                    pack.unlink()
+            pick.do_transfer()
