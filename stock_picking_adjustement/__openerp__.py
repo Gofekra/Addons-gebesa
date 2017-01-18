e6d6ccb931d4e3a627092103c9f33f3ec198ab6c
Deysy <damp_0508@hotmail.com>
2017-01-18 13:42:11 -0600

    [FIX] Change name in the views

---
+++ stock_picking_adjustement/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking Adjustement",
+    "summary": "Adds the menu for Input and Output by Adjustment.",
+    "version": "9.0.1.0.0",
+    "category": "Stock",
+    "website": "https://odoo-community.org/",
+    "author": "<Deysy Mascorro>, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base",
+        "stock",
+        "sale",
+    ],
+    "data": [
+        "views/",
+        "views/",
+        "views/",
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
