1f94660eabbad7892661ffd5a925f611402b8f6f
samuel barron <samuel.barron@gebesa.com>
2017-01-20 13:50:16 -0600

    [IMP] report complet

--- sale_order_everywhere/models/__init__.py
+++ sale_order_everywhere/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # © <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import procurement_group
 from . import procurement_order
 from . import stock_move
 from . import mrp_production
