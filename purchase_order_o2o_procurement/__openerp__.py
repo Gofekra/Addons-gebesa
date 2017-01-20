abd756d0223b2aac23ae4953f05e0ee9c59edb90
Cesar Barron <cesar.barron@gmail.com>
2017-01-20 13:06:41 -0600

    [ADD] Module for splitting PO by Procurement origin

---
+++ purchase_order_o2o_procurement/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# © 2017 Cesar Barron
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Order by Procurement's Origin",
+    "summary": "This module avoids odoo groups diferent proc's origins in PO",
+    "version": "9.0.1.0.0",
+    "category": "Custom",
+    "website": "https://odoo-community.org/",
+    "author": "Cesar Barron, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base",
+        "purchase",
+        "stock",
+        "mrp"
+    ],
+    "data": [
+        # "view/some_model_view.xml",
+    ],
+    "demo": [
+        # "demo/res_partner_demo.xml",
+    ],
+    "qweb": [
+        # "static/src/xml/module_name.xml",
+    ]
+}
