fee23819566c0d16884fc64a0d437ce7186aadaf
aldo <aldo.nerio@gmail.com>
2017-02-27 17:51:12 -0600

    se agrego el modulo de product_product_mu en la vista es table of margin of unity

---
+++ product_product_mu/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Product Mu",
+    "summary": "Module product mu",
+    "version": "9.0.1.0.0",
+    "category": "product",
+    "website": "https://odoo-community.org/",
+    "author": "<Aldo Nerio>, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/product_product_mu.xml",
+        "security/product_product_mu_security.xml",
+        "security/ir.model.access.csv"
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
