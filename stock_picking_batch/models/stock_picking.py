a79a523328740ef7d537f5830a05370beb72dd23
samuel barron <samuel.barron@gebesa.com>
2017-03-14 18:05:44 -0600

    [IMP] commit inherit force_transfer

--- stock_picking_batch/models/stock_picking.py
+++ stock_picking_batch/models/stock_picking.py
@@ -8,9 +8,9 @@ from openerp import fields, models
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    def force_transfer(self, force_qty=True):
-        for pick in self:
-            for pack in pick.pack_operation_ids:
-                if not pack.qty_done > 0:
-                    pack.unlink()
-            pick.do_transfer()
+    # def force_transfer(self, force_qty=True):
+    #     for pick in self:
+    #         for pack in pick.pack_operation_ids:
+    #             if not pack.qty_done > 0:
+    #                 pack.unlink()
+    #         pick.do_transfer()
