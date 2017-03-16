3190f26005a61353af584c48f08d78b6e7704af2
aldo <aldo.nerio@gmail.com>
2017-03-16 16:31:09 -0600

    Se actualizo el archivo mrp_production.py, en donde se le dio el atributo de copy como falso y buscar por ciudad

--- sale_order_everywhere/models/mrp_production.py
+++ sale_order_everywhere/models/mrp_production.py
@@ -11,18 +11,23 @@ class MrpProduction(models.Model):
     sale_id = fields.Many2one(
         'sale.order',
         string='Sale order',
+        copy=False,
     )
     partner_id = fields.Many2one(
         related='sale_id.partner_id',
         string='Customer',
         store=True,
+        copy=False,
     )
     client_order_ref = fields.Char(
         related='sale_id.client_order_ref',
         string='Customer ref',
         store=True,
+        copy=False,
     )
     city_shipping = fields.Char(
         related='sale_id.partner_shipping_id.city',
         string='City',
+        store=True,
+        copy=False,
     )
