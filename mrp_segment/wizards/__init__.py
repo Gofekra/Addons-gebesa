dc876a2e64edb8f6ae6745baf7bbe5aa2ab71a64
samuel barron <samuel.barron@gebesa.com>
2017-03-16 18:31:02 -0600

    [IMP] add production in state confirm

--- mrp_segment/wizards/__init__.py
+++ mrp_segment/wizards/__init__.py
@@ -2,3 +2,4 @@
 # © <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import mrp_production_segment
+from . import mrp_segment_add_production
---
+++ mrp_segment/wizards/mrp_segment_add_production.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import _, api, fields, models
+
+
+class MrpSegmentAddProduction(models.TransientModel):
+    _name = 'mrp.segment.add.production'
+
+    location_id = fields.Many2one(
+        'stock.location',
+        string=_('Segment location'),
+    )
+    production_ids = fields.Many2many(
+        'mrp.production',
+        string=('Production'),
+    )
+
+    @api.multi
+    def add_production(self):
+        segment_obj = self.env['mrp.segment']
+        segment_line_obj = self.env['mrp.segment.line']
+        active_ids = self._context.get('active_ids', []) or []
+        segment = segment_obj.browse(active_ids)
+        for produ in self.production_ids:
+            segment_line_obj.create({
+                'segment_id': segment.id,
+                'mrp_production_id': produ.id,
+                'product_id': produ.product_id.id,
+                'sale_name': produ.origin,
+                'product_qty': produ.product_qty,
+                'quantity': 0
+            })
