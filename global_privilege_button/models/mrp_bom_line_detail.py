8a6bac1a0a7ee08e2a10298f4a1f1cdf37f677d7
Deysy <damp_0508@hotmail.com>
2017-04-04 13:22:21 -0500

    (FIX) add new class

---
+++ global_privilege_button/models/mrp_bom_line_detail.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, _, models
+from openerp.exceptions import UserError
+
+
+class MrpBomLineDetail(models.Model):
+    _inherit = 'mrp.bom.line.detail'
+
+    @api.model
+    def create(self, vals):
+        if not self.env.user.has_group(
+                'global_privilege_button.group_manager_ldm'):
+            raise UserError(_('Error!\nYou do not have privileges to Create'
+                              ' Material(s) list.\nCheck with your'
+                              ' System Administrator.'))
+        return super(MrpBomLineDetail, self).create(vals)
+
+    @api.one
+    def write(self, vals):
+        if not self.env.user.has_group(
+                'global_privilege_button.group_manager_ldm'):
+            raise UserError(_('Error!\nYou do not have privileges to Modify'
+                              ' Material(s) list.\nCheck with your'
+                              ' System Administrator.'))
+        return super(MrpBomLineDetail, self).write(vals)
+
+    @api.multi
+    def unlink(self):
+        if not self.env.user.has_group(
+                'global_privilege_button.group_manager_ldm'):
+            raise UserError(_('Error!\nYou do not have privileges to Delete'
+                              ' Material(s) list.\nCheck with your'
+                              ' System Administrator.'))
+        return super(MrpBomLineDetail, self).unlink()
