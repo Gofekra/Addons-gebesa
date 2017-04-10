5f2e9038aa5f6f9e4a854d9173c5cafce915ae21
samuel barron <samuel.barron@gebesa.com>
2017-04-10 11:35:06 -0500

    [IMP] field individual_name

---
+++ product_structure_gebesa/models/product_product.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    individual_name = fields.Char(
+        string='Individual name',
+        translate=True,
+    )
