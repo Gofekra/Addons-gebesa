d7340d3a0da02dc54b10b120b1107949be55d310
samuel barron <samuel.barron@gebesa.com>
2017-04-27 18:34:39 -0500

    [IMP] change relate for onchange

--- account_analytic_everywhere/models/purchase_order.py
+++ account_analytic_everywhere/models/purchase_order.py
@@ -2,7 +2,7 @@
 # © <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models, _
+from openerp import api, fields, models, _
 
 
 class PurchaseOrder(models.Model):
@@ -10,7 +10,13 @@ class PurchaseOrder(models.Model):
 
     account_analytic_id = fields.Many2one(
         'account.analytic.account',
-        related='picking_type_id.warehouse_id.account_analytic_id',
+        # related='picking_type_id.warehouse_id.account_analytic_id',
         string=_(u'Analytic Account'),
         store=True,
     )
+
+    @api.onchange('picking_type_id')
+    def _onchange_picking_type_id(self):
+        super(PurchaseOrder, self)._onchange_picking_type_id()
+        warehouse = self.picking_type_id.warehouse_id
+        self.account_analytic_id = warehouse.account_analytic_id.id
--- stock_tree_views/models/purchase_order.py
+++ stock_tree_views/models/purchase_order.py
@@ -2,7 +2,7 @@
 # © <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models, _
+from openerp import api, fields, models, _
 
 
 class PurchaseOrder(models.Model):
@@ -10,7 +10,13 @@ class PurchaseOrder(models.Model):
 
     warehouse_id = fields.Many2one(
         'stock.warehouse',
-        related='picking_type_id.warehouse_id',
+        # related='picking_type_id.warehouse_id',
         string=_('Warehouse'),
         store=True,
     )
+
+    @api.onchange('picking_type_id')
+    def _onchange_picking_type_id(self):
+        super(PurchaseOrder, self)._onchange_picking_type_id()
+        warehouse = self.picking_type_id.warehouse_id
+        self.warehouse_id = warehouse.id
