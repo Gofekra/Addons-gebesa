edd38f8132b16b2b0df65b7a31c39abf8372d3e4
samuel barron <samuel.barron@gebesa.com>
2017-05-02 13:53:57 -0500

    [IMP] tranfer internal

---
+++ account_payment_transfer/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Payment transfer",
+    "summary": "Payment transfer",
+    "version": "9.0.1.0.0",
+    "category": "account",
+    "website": "https://odoo-community.org/",
+    "author": "<Samuel Barron>, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base",
+        "account_operating_unit",
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
---
+++ account_payment_transfer/models/account_payment.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import _, fields, models
+
+
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
+
+    def _get_dst_liquidity_aml_dict_vals(self):
+        dst_liquidity_aml_dict = {
+            'name': _('Transfer from %s') % self.journal_id.name,
+            'account_id':
+                self.destination_journal_id.default_credit_account_id.id,
+            'currency_id': self.destination_journal_id.currency_id.id,
+            'payment_id': self.id,
+            'journal_id': self.destination_journal_id.id,
+        }
+
+        if self.currency_id != self.company_id.currency_id:
+            dst_liquidity_aml_dict.update({
+                'currency_id': self.currency_id.id,
+                # 'amount_currency': -self.amount,
+            })
+
+        dst_liquidity_aml_dict.update({
+            'operating_unit_id':
+                self.destination_journal_id.operating_unit_id.id or False})
+        return dst_liquidity_aml_dict
