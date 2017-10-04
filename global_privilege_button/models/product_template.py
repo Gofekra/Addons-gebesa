2bde58ce98e3921ae2c33b1cf3f680f8de6c400b
cttox <cesar.barron@gmail.com>
2017-10-04 17:19:44 -0500

    Modify validation

--- global_privilege_button/models/product_template.py
+++ global_privilege_button/models/product_template.py
@@ -34,10 +34,32 @@ class ProductTemplate(models.Model):
             if vals.get('active') is False and self.qty_available != 0:
                 raise UserError(_('Error!\nNo puede Inactivar un Producto con'
                                   ' existencia en el Sistema.'))
-        if 'is_line' in vals.keys() and not self.env.user.has_group('global_privilege_button.group_manager_is_line_product'):
-            raise UserError(_('Error!\nYou do not have privileges to Modify Line Products.'))
-        if self.is_line is True and not self.env.user.has_group('global_privilege_button.group_manager_is_line_product'):
-            raise UserError(_('Error!\nYou do not have privileges to Modify Line Products.'))
+
+        # reference_mask,attribute_line_ids,name,sale_ok,purchase_ok,type,default_code,product_service_id,is_line,family_id,group_id,line_id,route_ids,categ_id,tracking,invoice_policy,description_sale,standard_price
+        criticalFields = {'reference_mask', 'attribute_line_ids',
+                          'name', 'sale_ok', 'purchase_ok', 'type',
+                          'product_service_id', 'is_line', 'family_id',
+                          'group_id', 'line_id', 'route_ids', 'categ_id',
+                          'tracking', 'invoice_policy',
+                          'description_sale', 'standard_price', 'default_code'}
+
+        needVal = False
+        for field in criticalFields:
+            if field in vals.keys():
+                needVal = True
+
+        # if len(vals.keys()) > 0:
+        if needVal:
+            if 'is_line' in vals.keys() and not self.env.user.has_group(
+                    'global_privilege_button.group_manager_is_line_product'):
+                raise UserError(_('Error!\nYou do not have privileges \
+                                to Set Line Products.'))
+
+            if self.is_line is True and not self.env.user.has_group(
+                    'global_privilege_button.group_manager_is_line_product'):
+                raise UserError(_('Error!\nYou do not have privileges \
+                                to Modify Line Products.\n You are \
+                                modifying: %s') % (vals))
 
         return super(ProductTemplate, self).write(vals)
 
