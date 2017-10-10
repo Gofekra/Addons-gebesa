735ba6d2afa2c11e3d5b3d0ccd62621cc5f3e4b5
Samuel <samuel.barron@gebesa.com>
2017-10-10 17:38:58 -0500

    [IMP] compute data sale

--- sale_order_gebesa/models/sale_order_line.py
+++ sale_order_gebesa/models/sale_order_line.py
@@ -18,27 +18,36 @@ class SaleOrderLine(models.Model):
     net_sale = fields.Float(
         string=_(u'Net sales'),
         digits_compute=dp.get_precision('Account'),
+        compute='_compute_profit_margin',
+        store=True
     )
 
     freight_amount = fields.Float(
         string=_(u'Freight amount'),
         digits_compute=dp.get_precision('Account'),
+        compute='_compute_profit_margin',
+        store=True
     )
 
     installation_amount = fields.Float(
         string=_(u'installation amount'),
         digits_compute=dp.get_precision('Account'),
+        compute='_compute_profit_margin',
+        store=True
     )
 
     standard_cost = fields.Float(
         string=_(u'Standard cost'),
         digits_compute=dp.get_precision('Account'),
+        compute='_compute_profit_margin',
+        store=True
     )
 
     profit_margin = fields.Float(
         string=_(u'Profit margin'),
         digits_compute=dp.get_precision('Account'),
-        compute='_compute_profit_margin'
+        compute='_compute_profit_margin',
+        store=True
     )
 
     low_mu = fields.Boolean(
@@ -62,11 +71,9 @@ class SaleOrderLine(models.Model):
     #      muline = record.product_id.product_templ_id.line_id.mu.min
     #      if muline > 0:
 
-    @api.depends('profit_margin', 'price_unit', 'order_id.perc_freight', 'order_id.perc_installation')
+    @api.depends('profit_margin', 'price_unit', 'order_id.perc_freight',
+                 'order_id.perc_installation')
     def _compute_low_mu(self):
-        # ---> Set BreakPoint
-        #import pdb;
-        # pdb.set_trace()
         for record in self:
             mugroup = record.product_id.product_tmpl_id.group_id.mu_min
             muline = record.product_id.product_tmpl_id.line_id.mu_min
@@ -77,25 +84,13 @@ class SaleOrderLine(models.Model):
                 if muline > record.profit_margin:
                     record.low_mu = True
 
-    @api.onchange('price_unit', 'product_uom_qty', 'order_id.perc_freight', 'order_id.perc_installation')
+    @api.depends('price_unit', 'product_uom_qty', 'order_id.perc_freight',
+                 'order_id.perc_installation')
     def _compute_profit_margin(self):
-        # ---> Set BreakPoint
-        # import pdb;
-        # pdb.set_trace()
-        # global_cost = 0.0
-        # global_net_sale = 0.0
-        # global_freight = 0.0
-        # global_installa = 0.0
-        # global_profit_margin = 0.0
         for record in self:
             currency = record.order_id.company_id.currency_id
             product = record.product_id
             standard_cost = product.standard_price or 0.0
-            # standard_cost = currency.compute(
-            #    standard_cost, record.order_id.pricelist_id.currency_id) or 0.0
-            # if standard_cost > 0:
-            #    standard_cost = standard_cost
-            #    * inv.rate
             total_cost = standard_cost * record.product_uom_qty
             perc_freight = record.order_id.perc_freight or False
             freight = 0.0
@@ -123,30 +118,3 @@ class SaleOrderLine(models.Model):
             record.profit_margin = profit_margin
             record.purchase_price = standard_cost
             record.standard_cost = total_cost
-
-            # global_cost += total_cost
-            # global_net_sale += net_sale
-            # global_freight += freight
-            # global_installa += installation
-
-        # if global_net_sale > 0.000000:
-        #   global_profit_margin = (1 - (global_cost) / global_net_sale)
-        #  global_profit_margin = global_profit_margin * 100
-
-        # record.total_cost = global_cost
-        # record.total_net_sale = global_net_sale
-        # record.total_freight = global_freight
-        # record.total_installation = global_installa
-        # record.profit_margin = global_profit_margin
-
-            # fulfilled = fields.Float(
-            #     string=_(u'Fulfilled'),
-            #     digits_compute=dp.get_precision('Account'),
-            #     help=_(u'Fulfilled'),
-            # )
-
-            # invoiced = fields.Float(
-            #     string=_(u'Invoiced'),
-            #     digits_compute=dp.get_precision('Account'),
-            #     help=_(u'Invoiced')
-            # )
