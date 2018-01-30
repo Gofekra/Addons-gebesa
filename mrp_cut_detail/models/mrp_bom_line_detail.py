e5726329216d488275a553094551539ff82fd833
cttox <cesar.barron@gmail.com>
2018-01-30 16:27:53 -0600

    Make row not copiable

--- mrp_cut_detail/models/mrp_bom_line_detail.py
+++ mrp_cut_detail/models/mrp_bom_line_detail.py
@@ -41,6 +41,7 @@ class MrpBomLineDetail(models.Model):
     row = fields.Integer(
         _('Row'),
         default=_default_row,
+        copy=False,
         help=_('Gives the row order when displaying.'),
     )
 
